8406ea04e0d42cd43642c9834c42846c42fac40a
CamilaShi <38038501+CamilaShi@users.noreply.github.com>
2026-09-23 17:25:18 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Cole aqui a URL do seu Apps Script depois de implantar como app da web.
 // Ela termina assim: https://script.google.com/macros/s/ALGO_LONGO/exec
-const API_URL = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
+const API_URL = "https://script.google.com/macros/s/AKfycbxaXEmdWCwEmh9xjuloz9phq0AWr80VhXrWYCRB5_sqTOoP1kOjdczJB3Jdkk4uGm9e8Q/exec";
