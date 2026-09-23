3cac22ba16e3e6890ecde3e006ad37480260c37a
CamilaShi <38038501+CamilaShi@users.noreply.github.com>
2026-09-23 14:51:32 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,3 +0,0 @@
-// Cole aqui a URL do seu Apps Script depois de implantar como app da web.
-// Ela termina assim: https://script.google.com/macros/s/ALGO_LONGO/exec
-const API_URL = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
